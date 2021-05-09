f9e97c06b8bbd6ddf9e6773d78a0a1c94dbec22c
Shivapreyah <58418780+Shivapreyah@users.noreply.github.com>
2021-05-09 07:55:31 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AdminUI from './AdminUI';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AdminUI />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
